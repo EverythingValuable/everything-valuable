9c40fe54794b772438fcd15b86dc566f79ac0108
Michael Fallon <mikefallonphotos@gmail.com>
2026-03-24 17:31:19 +0000

    File changes

--- src/components/product/BidSection.jsx
+++ src/components/product/BidSection.jsx
@@ -443,7 +443,7 @@ export default function BidSection({ item }) {
             </div>
             <div className="flex justify-between text-xs text-muted-foreground">
               <span>Remaining due after upfront payment</span>
-              <span>${(price + serviceFee - feeCredit - serviceFee).toLocaleString("en-US", { minimumFractionDigits: 2 })}</span>
+              <span>${(price - feeCredit).toLocaleString("en-US", { minimumFractionDigits: 2 })}</span>
             </div>
           </div>
 
