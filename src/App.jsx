dae889d0feee44455e362d0b11169054b9e611dc
Michael Fallon <mikefallonphotos@gmail.com>
2026-03-25 13:24:38 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -72,6 +72,7 @@ function App() {
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
         <Router>
+          <ScrollToTop />
           <AuthenticatedApp />
         </Router>
         <Toaster />
