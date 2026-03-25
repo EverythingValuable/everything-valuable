627758f2d52a74c51cd926d362e29857c7b6ed9c
Michael Fallon <mikefallonphotos@gmail.com>
2026-03-25 14:22:39 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -17,7 +17,7 @@ export default function HeroSection() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrent(prev => (prev + 1) % heroImages.length);
-    }, 3500);
+    }, 5000);
     return () => clearInterval(interval);
   }, []);
 
