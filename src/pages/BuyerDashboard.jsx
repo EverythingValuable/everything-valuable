29d30934161ee91393773f3d9789d26417583f70
Michael Fallon <mikefallonphotos@gmail.com>
2026-05-05 16:16:16 +0000

    File changes

--- src/pages/BuyerDashboard.jsx
+++ src/pages/BuyerDashboard.jsx
@@ -198,7 +198,7 @@ export default function BuyerDashboard() {
 
   return (
     <div className="min-h-screen bg-[hsl(40,20%,97%)]">
-      <div className="max-w-6xl mx-auto px-4 md:px-8 py-10">
+      <div className="max-w-[1400px] mx-auto px-4 md:px-6 lg:px-8 py-10">
 
         {/* Page Header */}
         <div className="mb-8">
